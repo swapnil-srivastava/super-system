a9bc582aee8c3ab6c52dc457aa6d686605919caa
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-12-13 13:17:12 +0000

    first commit

---
+++ swapnil-srivastava/src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplication.java
@@ -0,0 +1,13 @@
+package eu.swapnilsrivastava.swapnilsrivastava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapnilSrivastavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapnilSrivastavaApplication.class, args);
+	}
+
+}
